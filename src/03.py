d269a451485ce723e98e1fcc184bdb37c82c8ea6
Homnibus <eseunes@gmail.com>
2023-12-03 15:44:52 +0100

    Day 3 - 1/2 with outline

--- src/03.py
+++ src/03.py
@@ -28,13 +28,18 @@ def is_symbol(character: str) -> bool:
 schematic: list[SchematicLine] = []
 
 # Input parsing
+# add first empty line
+schematic.append(SchematicLine(0, "." * (schematic_len + 2), []))
 for line_index, line in enumerate(input_str.splitlines()):
+    line = "." + line + "."
     current_part_list = []
     for match in re.finditer(r"[0-9]+", line):
         current_part = Part(int(match.group(0)), match.start(), match.end())
         current_part_list.append(current_part)
-    current_schematic_line = SchematicLine(line_index, line, current_part_list)
+    current_schematic_line = SchematicLine(line_index + 1, line, current_part_list)
     schematic.append(current_schematic_line)
+# add last empty line
+schematic.append(SchematicLine(-1, "." * (schematic_len + 2), []))
 
 
 # Filter data and add to total
@@ -42,62 +47,60 @@ total = 0
 
 for schematic_line in schematic:
     for part in schematic_line.parts:
-        # previous line
-        if schematic_line.line_number != 0:
-            # over the part number
-            previous_line = schematic[schematic_line.line_number - 1]
-            touched = False
-            for character in previous_line.line_value[part.start : part.end]:
-                if is_symbol(character):
-                    touched = True
-                    break
-            if touched:
-                total += part.value
-                continue
-            # diagonal before
-            if part.start > 0 and is_symbol(previous_line.line_value[part.start - 1]):
-                total += part.value
-                continue
-            # diagonal after
-            if part.end < (schematic_len - 1) and is_symbol(
-                previous_line.line_value[part.end]
-            ):
-                total += part.value
-                continue
+        # Previous line
+        # over the part number
+        previous_line = schematic[schematic_line.line_number - 1]
+        touched = False
+        for character in previous_line.line_value[part.start : part.end]:
+            if is_symbol(character):
+                touched = True
+                break
+        if touched:
+            total += part.value
+            continue
+        # diagonal before
+        if is_symbol(previous_line.line_value[part.start - 1]):
+            total += part.value
+            continue
+        # diagonal after
+        if is_symbol(
+            previous_line.line_value[part.end]
+        ):
+            total += part.value
+            continue
 
-        # current line
+        # Current line
         # before
-        if part.start > 0 and is_symbol(schematic_line.line_value[part.start - 1]):
+        if is_symbol(schematic_line.line_value[part.start - 1]):
             total += part.value
             continue
         # after
-        if part.end < (schematic_len - 1) and is_symbol(
+        if is_symbol(
             schematic_line.line_value[part.end]
         ):
             total += part.value
             continue
 
-        # next line
-        if schematic_line.line_number != (len(schematic) - 1):
-            # under the part number
-            next_line = schematic[schematic_line.line_number + 1]
-            touched = False
-            for character in next_line.line_value[part.start : part.end]:
-                if is_symbol(character):
-                    touched = True
-                    break
-            if touched:
-                total += part.value
-                continue
-            # diagonal before
-            if part.start > 0 and is_symbol(next_line.line_value[part.start - 1]):
-                total += part.value
-                continue
-            # diagonal after
-            if part.end < (schematic_len - 1) and is_symbol(
-                next_line.line_value[part.end]
-            ):
-                total += part.value
-                continue
-            
+        # Next line
+        # under the part number
+        next_line = schematic[schematic_line.line_number + 1]
+        touched = False
+        for character in next_line.line_value[part.start : part.end]:
+            if is_symbol(character):
+                touched = True
+                break
+        if touched:
+            total += part.value
+            continue
+        # diagonal before
+        if is_symbol(next_line.line_value[part.start - 1]):
+            total += part.value
+            continue
+        # diagonal after
+        if is_symbol(
+            next_line.line_value[part.end]
+        ):
+            total += part.value
+            continue
+
 print(total)
