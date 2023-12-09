3fa3951ddbe88baead8175f737ebbbc9efbd8f31
Homnibus <eseunes@gmail.com>
2023-12-09 09:57:45 +0100

    Day 9 - 2/2

--- src/09.py
+++ src/09.py
@@ -19,17 +19,17 @@ def is_full_of_zero(values: list[int]) -> bool:
             return False
     return True
 
-def find_next_value(values: list[int]) -> int:
+def find_previous_value(values: list[int]) -> int:
     list_of_list = [values]
     current_list = values
     while not is_full_of_zero(current_list): 
         current_list = create_next_values(current_list)
         list_of_list.append(current_list)
         
-    next_value = 0
+    previous_value = 0
     for i in reversed(range(0,len(list_of_list)-1)):
-        next_value += list_of_list[i][-1]
-    return next_value
+        previous_value = list_of_list[i][0] - previous_value
+    return previous_value
 
 input_str = get_input(9)
 splited_lines = input_str.splitlines()
@@ -37,6 +37,6 @@ values_list = extract_values(splited_lines)
 total = 0 
 
 for values in values_list:
-   total += find_next_value(values) 
+   total += find_previous_value(values) 
     
 print(total)
