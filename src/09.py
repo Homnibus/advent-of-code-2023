a06a2d31e7c372ac65f9c0185b418ecfa271f5f2
Homnibus <eseunes@gmail.com>
2023-12-09 09:54:58 +0100

    Day 9 - 1/2

---
+++ src/09.py
@@ -0,0 +1,42 @@
+import re
+from utils.api import get_input
+
+def extract_values(input: list[str]) -> list[list[int]]:
+    values_list: list[list[int]] = []
+    for line in input:
+        values_list.append([int(value) for value in re.findall(r"-?\d+", line)])
+    return values_list    
+    
+def create_next_values(values: list[int]) -> list[int]:
+    next_values: int = []
+    for i in range(len(values) - 1):
+        next_values.append(values[i+1] - values[i])
+    return next_values
+
+def is_full_of_zero(values: list[int]) -> bool:
+    for value in values:
+        if value!= 0:
+            return False
+    return True
+
+def find_next_value(values: list[int]) -> int:
+    list_of_list = [values]
+    current_list = values
+    while not is_full_of_zero(current_list): 
+        current_list = create_next_values(current_list)
+        list_of_list.append(current_list)
+        
+    next_value = 0
+    for i in reversed(range(0,len(list_of_list)-1)):
+        next_value += list_of_list[i][-1]
+    return next_value
+
+input_str = get_input(9)
+splited_lines = input_str.splitlines()
+values_list = extract_values(splited_lines)
+total = 0 
+
+for values in values_list:
+   total += find_next_value(values) 
+    
+print(total)
