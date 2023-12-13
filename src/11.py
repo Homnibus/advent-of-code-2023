8dcdebd10196de6c954c456199ca779ef336bd97
Homnibus <eseunes@gmail.com>
2023-12-13 11:30:52 +0100

    Day 11 - 1/2 without padding

--- src/11.py
+++ src/11.py
@@ -4,14 +4,6 @@ from utils.api import get_input
 def rotate(map: list[str]) -> list[str]:
     return [''.join([map[j][i] for j in range(len(map))]) for i in range(len(map[0]))]
 
-def add_padding(map: list[str]) -> list[str]:
-    padded_map = map.copy()
-    for i in range(0, len(padded_map)):
-        padded_map[i] = "." + padded_map[i] + "."
-    padded_map.append("." * len(map[0]))
-    padded_map.insert(0, "." * len(map[0]))
-    return padded_map
-
 def expend_empty_lines(map: list[str]) -> list[str]:
     line_expended_map: list[str] = []
     for line in map:
@@ -28,8 +20,7 @@ def expend_empty_col(map: list[str]) -> list[str]:
 
 input_str = get_input(11)
 map = input_str.splitlines()
-padded_map = add_padding(map)
-expended_map = add_padding(expend_empty_lines(expend_empty_col(map)))
+expended_map = expend_empty_lines(expend_empty_col(map))
 
 galaxys:list[list[int]] = []
 
