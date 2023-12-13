7d2f427162bf78b06afd9442984caadd2612d289
Homnibus <eseunes@gmail.com>
2023-12-13 11:45:01 +0100

    Day 11 - 2/2

--- src/11.py
+++ src/11.py
@@ -4,27 +4,27 @@ from utils.api import get_input
 def rotate(map: list[str]) -> list[str]:
     return [''.join([map[j][i] for j in range(len(map))]) for i in range(len(map[0]))]
 
-def expend_empty_lines(map: list[str]) -> list[str]:
-    line_expended_map: list[str] = []
-    for line in map:
+def get_lines_to_expand(map: list[str]) -> list[int]:
+    lines_to_expand:list[int] = []
+    for line_index,line in enumerate(map):
         if "#" not in line:
-            line_expended_map.append("." * len(map[0]))
-        line_expended_map.append(line)
-    return line_expended_map
-    
-def expend_empty_col(map: list[str]) -> list[str]:
+            lines_to_expand.append(line_index)
+    return lines_to_expand
+
+def get_columns_to_expand(map: list[str]) -> list[int]:
     map_rotated_90 = rotate(map)
-    col_expended_map_rotated_90 = expend_empty_lines(map_rotated_90)
-    col_expended_map = rotate(col_expended_map_rotated_90)
-    return col_expended_map
+    return get_lines_to_expand(map_rotated_90)
 
 input_str = get_input(11)
 map = input_str.splitlines()
-expended_map = expend_empty_lines(expend_empty_col(map))
+lines_to_expand = get_lines_to_expand(map)
+columns_to_expand = get_columns_to_expand(map)
+
+expand_value = 1000000
 
 galaxys:list[list[int]] = []
 
-for line_index, line in enumerate(expended_map):
+for line_index, line in enumerate(map):
     for match in re.finditer(r"#", line):
         galaxys.append([line_index,match.start()])
     
@@ -33,6 +33,8 @@ total = 0
 while len(galaxys) > 0:
     current_galaxy = galaxys.pop(0)
     for galaxy in galaxys:
-        total += abs(current_galaxy[0] - galaxy[0]) + abs(current_galaxy[1] - galaxy[1])
+        line_expand = [line for line in lines_to_expand if (current_galaxy[0] > line > galaxy[0]) or (current_galaxy[0] < line < galaxy[0]) ]
+        column_expand = [column for column in columns_to_expand if (current_galaxy[1] > column > galaxy[1]) or (current_galaxy[1] < column < galaxy[1]) ]
+        total += abs(current_galaxy[0] - galaxy[0]) + len(line_expand) * (expand_value - 1)  + abs(current_galaxy[1] - galaxy[1]) + len(column_expand) * (expand_value - 1)
         
 print(total)
